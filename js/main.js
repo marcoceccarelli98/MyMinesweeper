b48408eb8e48cd082ff9afc4bfd6959f2dd71e99
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-14 22:31:00 +0200

    [UPD] 1) added a mines generator function 2)added auto box dimention on resize (to fix)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 // document.addEventListener('contextmenu', event => event.preventDefault());
 
 const gameContainer = document.querySelector('.game-container');
+const selectorSize = document.getElementById('size-select');
 
 // ----- GAME CONST -----
 //Row
@@ -14,20 +15,29 @@ const boardSmallCol = 10;
 const boardMediumCol = 15;
 const boardLargeCol = 16;
 
+const mineField = [];
+const mines = [];
+
 // ----- GAME VARIABLE -----
 let row = boardSmallRow;
 let col = boardSmallCol;
 
-addGridTemplateCol();
-generateBoard();
+//----------------------// 
+//                      //
+//      MAIN GAME       //
+//                      //
+//----------------------//
+
+// Initialization
+iniRowColSize();
 iniBtnPlay();
 iniSizeSelector();
-getBoardSize();
+iniScreenResize();
 
 
-let mineField = [];
-
-
+addGridTemplateCol();
+generateBoard();
+newMineField();
 
 
 //----------------------// 
@@ -36,29 +46,24 @@ let mineField = [];
 //                      //
 //----------------------//
 
-function addGridTemplateCol() {
-    gameContainer.removeAttribute('style');
-    gameContainer.setAttribute('style', 'grid-template-columns: repeat(' + col.toString() + ' , 1fr);');
+//----------------
+// Initialization
+//----------------
+
+function iniScreenResize() {
+    window.addEventListener('resize', function () {
+        // Here's the code to execute when the window is resized
+        const cell = document.querySelectorAll('.box');
+
+        cell.forEach(function (cell) {
+            resizeCells(cell, cellDim());
+        });
+    });
 }
 
-function resetBoard() {
-    gameContainer.innerHTML = '';
-}
-
-function generateBoard() {
-
-    resetBoard();
-    addGridTemplateCol();
-
-    let c = 1; //debug
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            const cell = document.createElement('button');
-            cell.classList.add('box');
-            cell.innerText = c++; //debug
-            gameContainer.append(cell);
-        }
-    }
+// Row and Col value initialization on first start and refresh
+function iniRowColSize() {
+    getBoardSize(selectorSize.value);
 }
 
 // PLAY BUTTON
@@ -73,27 +78,111 @@ function iniBtnPlay() {
 function iniSizeSelector() {
     const selectorSize = document.getElementById('size-select');
     selectorSize.addEventListener('change', function () {
-        switch (selectorSize.value) {
-            case 'small':
-                row = boardSmallRow;
-                col = boardSmallCol;
-                break;
-            case 'medium':
-                row = boardMediumRow;
-                col = boardMediumCol;
-                break;
-            case 'large':
-                row = boardLargeRow;
-                col = boardLargeCol;
-                break;
-        }
+        getBoardSize(selectorSize.value);
     });
 }
 
-// for (let i = 0; i < row; i++) {
-//     MineField[i] = [];
-//     for (let j = 0; j < col; j++) {
-//         MineField[i][j] = 0;
+//----------------
+//     Board
+//----------------
 
-//     }
-// }
+function getBoardSize(size) {
+    switch (size) {
+        case 'small':
+            row = boardSmallRow;
+            col = boardSmallCol;
+            break;
+        case 'medium':
+            row = boardMediumRow;
+            col = boardMediumCol;
+            break;
+        case 'large':
+            row = boardLargeRow;
+            col = boardLargeCol;
+            break;
+    }
+}
+
+function addGridTemplateCol() {
+    gameContainer.style.gridTemplateColumns = `repeat(${col.toString()}, 1fr)`;
+}
+
+function cellDim() {
+    let attribute = '';
+    console.log(selectorSize.value);
+    if (window.innerWidth <= 500) {
+        attribute = `width : calc(100vw / ${col} - 2px)`;
+        return attribute
+    } else if (window.innerWidth > 500 && window.innerWidth < 1200) {
+        attribute = `width : calc(100vw / ${col} - 20px)`;
+        return attribute
+    } else {
+        attribute = `width : calc(100vh / ${row} - 5px)`;
+        return attribute
+    }
+}
+
+function resizeCells(cell, attribute) {
+    cell.removeAttribute('style');
+    cell.setAttribute('style', attribute);
+}
+
+function resetBoard() {
+    gameContainer.innerHTML = '';
+}
+
+function generateBoard() {
+
+    resetBoard();
+    addGridTemplateCol();
+
+    let c = 1; //--debug
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            const cell = document.createElement('button');
+            cell.classList.add('box');
+            resizeCells(cell, cellDim());
+            gameContainer.append(cell);
+        }
+    }
+    console.log(window.innerWidth);
+}
+
+//----------------
+//     Game
+//----------------
+
+function newMineField() {
+    minesGenerator(3);
+    for (let i = 0; i < row; i++) {
+        mineField[i] = [];
+        for (let j = 0; j < col; j++) {
+            for (let c = 0; c < mines.length; c++) {
+                if (arraysAreEqual(mines[c], [i, j])) {
+                    mineField[i][j] = 1;
+                    break;
+                } else {
+                    mineField[i][j] = 0;
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
+function minesGenerator(minesNum) {
+    for (let i = 0; i < minesNum; i++) {
+        mines[i] = [Math.floor(Math.random() * row), Math.floor(Math.random() * col)];
+    }
+    console.log(mines);
+}
+
+function arraysAreEqual(array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+    return array1.every((value, index) => value === array2[index]);
+}
